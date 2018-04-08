e73b8b151ff36d261528ce1f129ad7b46e53beb3
NEUBill <38072148+NEUBill@users.noreply.github.com>
2018-04-08 13:14:19 +0800

    Update ActivityDao

--- src/dao/ActivityDao.java
+++ src/dao/ActivityDao.java
@@ -1,10 +1,16 @@
 package dao;
 
+import model.Movement;
+
 public interface ActivityDao {
     /**
      * 根据用户名查找用户的动态
      *@param  name 用户名
      *@return 该用户的动态
      */
-    movement[]  findByName(String name);
+    Movement[]  findByName(String name);
+    /**
+     * 添加动态
+     */
+    void add (Movement movement);
 }
--- src/dao/impl/ActivityDaompl.java
+++ src/dao/impl/ActivityDaompl.java
@@ -1,9 +1,40 @@
 package dao.impl;
 
+import Util.DBUtil;
 import dao.ActivityDao;
+import model.Movement;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class ActivityDaompl implements ActivityDao {
     private Connection conn = null;
+
+    public void add(Movement movement){
+        try{
+            conn = DBUtil.connectDB();
+            PreparedStatement stm = conn.prepareStatement("INSERT INTO movement(name,thumb,comment) VALUES (?,?,?)");
+            stm.setString(1,movement.getNickname());
+            stm.setString(2,movement.getComment());
+            stm.setInt(3,movement.getThumb());
+            try {
+                stm.executeUpdate();
+                System.out.println("ActivityDao: 添加动态成功");
+            } catch (Exception e1) {
+                System.out.println("ActivityDao: 添加动态失败");
+            }
+            stm.close();
+            conn.close(); // 关闭数据库连接
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public movement[] findByName(String name) {
+        try {
+            conn = DBUtil.connectDB(); //连接数据库
+            PreparedStatement stm = conn.prepareStatement("SELECT * FROM movement");
+        }
+    }
+
 }
