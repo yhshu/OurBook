20e2438c89f0024429038702cf79cd3743c713fb
NEUBill <38072148+NEUBill@users.noreply.github.com>
2018-04-08 19:49:23 +0800

    update movement

---
+++ src/model/Activity.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Activity {
+    String nickname; //用户名
+    int action;
+    //action = 1 :
+
+    public Activity(String n, String c, int t){
+        this.nickname = n;
+    }
+}
