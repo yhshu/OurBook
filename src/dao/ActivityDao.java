31484a76c47a3f4fc6581279686117a5347ce1f2
Yiheng Shu <lzw429@qq.com>
2018-04-08 21:14:11 +0800

    before merge

--- src/dao/ActivityDao.java
+++ src/dao/ActivityDao.java
@@ -5,12 +5,14 @@ import model.Activity;
 public interface ActivityDao {
     /**
      * 根据用户名查找用户的动态
-     *@param  name 用户名
-     *@return 该用户的动态
+     *
+     * @param name 用户名
+     * @return 该用户的动态
      */
-    Activity[]  findByName(String name);
+    Activity[] findByName(String name);
+
     /**
      * 添加动态
      */
-    void add (Activity activity);
+    void add(Activity activity);
 }
