8c3bd11bb8d6a18bcb4508c8cf7227b77244b498
chipsbear <35371451+chipsbear@users.noreply.github.com>
2018-04-19 18:22:04 +0800

    可以关注

--- src/servlets/FollowServlet.java
+++ src/servlets/FollowServlet.java
@@ -6,6 +6,8 @@ import service.FollowService;
 import service.impl.FollowServiceImpl;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -16,13 +18,16 @@ import java.io.IOException;
  * @description: 关注用户、取消关注用户
  * @create: 04-18-20
  */
-public class FollowServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+@WebServlet("/FollowServlet")
+public class FollowServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
         System.out.println("****1");
     }
-
-    public void add(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("****1");
         response .setContentType("text/html");
         System.out.println("****2");
@@ -37,12 +42,12 @@ public class FollowServlet {
         String follower = request.getParameter("follower");
         try{
             followservice.addFollow(followee,follower);
-            System.out.println("BookServlet: 关注书目成功");
+            System.out.println("BookServlet: 关注成功");
             // 添加成功后，请求重定向，查看本书
            //response.sendRedirect("/homepage.jsp?id=" + bookDao.maxID());
         }catch (Exception e) {
             e.printStackTrace();
-            System.out.println("BookServlet: 添加书目失败");
+            System.out.println("BookServlet: 添加失败");
         }
 
     }
