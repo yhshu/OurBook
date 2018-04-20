20647fdf41389a02de1e9cd2ae7deaf4ae655389
Yiheng Shu <lzw429@qq.com>
2018-04-20 22:49:19 +0800

    update

--- src/servlets/HomepageServlet.java
+++ src/servlets/HomepageServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/home")
@@ -26,26 +27,24 @@ public class HomepageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("user");
+        HttpSession session = request.getSession();
         BookService bookService = new BookServiceImpl();
         UserService userService = new UserServiceImpl();
-        if (request.getSession().getAttribute("username") == null) // 未登录
-            response.sendRedirect("index.jsp");
-        if (username == null) username = (String) request.getSession().getAttribute("username");
-        String currentUser = (String) request.getSession().getAttribute("username");
+        String currentUser = (String) session.getAttribute("username");
         String redirect = "homepage.jsp";
-        if (currentUser == null) {
+        if (currentUser == null) { // 未登录
             currentUser = "";
             redirect = "login.jsp";
         }
-        if (username.equals(currentUser)) {
-            Book[] books = bookService.findByEditor(username);
-            User[] followers = userService.getFollowers(username);
-            User[] followees = userService.getFollowees(username);
-            request.setAttribute("books", books);
-            request.setAttribute("followers", followers);
-            request.setAttribute("followees", followees);
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(redirect);
-            requestDispatcher.forward(request, response);
-        }
+        if (username == null) // 表单参数中用户名为空，跳转到当前登录用户的主页
+            username = currentUser;
+        Book[] books = bookService.findByEditor(username);
+        User[] followers = userService.getFollowers(username);
+        User[] followees = userService.getFollowees(username);
+        request.setAttribute("books", books);
+        request.setAttribute("followers", followers);
+        request.setAttribute("followees", followees);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(redirect);
+        requestDispatcher.forward(request, response);
     }
 }
