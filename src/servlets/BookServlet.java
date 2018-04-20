458da60cb33143189434ff5e77d31efd692b3d9c
Yiheng Shu <lzw429@qq.com>
2018-04-20 09:33:50 +0800

    修复了添加章节时 PrintSteam 乱码问题

--- src/servlets/BookServlet.java
+++ src/servlets/BookServlet.java
@@ -34,17 +34,17 @@ public class BookServlet extends BaseServlet {
         String message = null;
         String filename = "";
         try {
-            if (ServletFileUpload.isMultipartContent(request)) {  //判断获取的是不是文件
+            if (ServletFileUpload.isMultipartContent(request)) {  // 判断获取的是不是文件
                 DiskFileItemFactory disk = new DiskFileItemFactory();
-                disk.setSizeThreshold(20 * 1024);      //设置内存可存字节数
-                disk.setRepository(disk.getRepository());  //设置临时文件目录
+                disk.setSizeThreshold(20 * 1024);      // 设置内存可存字节数
+                disk.setRepository(disk.getRepository());  // 设置临时文件目录
                 ServletFileUpload up = new ServletFileUpload(disk);
                 int maxsize = 2 * 1024 * 1024;
-                List list = up.parseRequest(request);   //获取上传列表
+                List list = up.parseRequest(request);   // 获取上传列表
                 for (Object aList : list) {
-                    FileItem fm = (FileItem) aList;//遍历列表
+                    FileItem fm = (FileItem) aList; // 遍历列表
                     if (!fm.isFormField()) {
-                        String filePath = fm.getName();  //获取文件全路径名
+                        String filePath = fm.getName();  // 获取文件全路径名
                         if (fm.getSize() > maxsize) {
                             message = "文件太大了，不要超过2MB";
                             break;
@@ -53,20 +53,25 @@ public class BookServlet extends BaseServlet {
                         filename = (bookDao.maxID() + 1) + extension;
                         File saveFile = new File(this.getServletContext().getRealPath("/resources/cover/")
                                 + filename);
-                        fm.write(saveFile);//向文件中写入数据
+                        fm.write(saveFile); // 向文件中写入数据
                         message = "文件上传成功！";
                     } else {
-                        String foename = fm.getFieldName();//获取表单元素名
-                        String con = fm.getString("GBK");
+                        String foename = fm.getFieldName(); // 获取表单元素名
+                        String con = fm.getString("UTF-8");
                         //表单元素
-                        if (foename.equals("bookName")) {
-                            bookName = con;
-                        } else if (foename.equals("keywords")) {
-                            keywords = con;
-                        } else if (foename.equals("bookDescription")) {
-                            bookDescription = con;
-                        } else if (foename.equals("editor")) {
-                            editor = con;
+                        switch (foename) {
+                            case "bookName":
+                                bookName = con;
+                                break;
+                            case "keywords":
+                                keywords = con;
+                                break;
+                            case "bookDescription":
+                                bookDescription = con;
+                                break;
+                            case "editor":
+                                editor = con;
+                                break;
                         }
                     }
                 }
